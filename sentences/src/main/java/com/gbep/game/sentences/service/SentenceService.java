0a0063c0cf1041ef2714c994da5519e8a8c82ddd
Anna Shavrina <shavrina.av@phystech.edu>
2021-09-07 19:25:08 +0300

    wiring sentences to master

--- sentences/src/main/java/com/gbep/game/sentences/service/SentenceService.java
+++ sentences/src/main/java/com/gbep/game/sentences/service/SentenceService.java
@@ -7,7 +7,6 @@ import com.gbep.game.sentences.repository.SentenceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.*;
 
 @Service
