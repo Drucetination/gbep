ab7614b7cf18dbdc2da68dcedfcc6083d6d25808
Anna Shavrina <shavrina.av@phystech.edu>
2021-09-07 12:05:12 +0300

    bot

---
+++ bot-api/src/test/java/com/gbep/botapi/BotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbep.botapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
